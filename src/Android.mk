MIPS/external-dnsmasq

Suppress unused-parameter/variable warnings in dnsmasq.
Bug: 26936282
Change-Id: Ie3c1644730dbfe705d5aa4ed0855206fa24a42c3

src/Android.mk
@@ -10,7 +10,8 @@ LOCAL_MODULE := dnsmasq
 
 LOCAL_C_INCLUDES := external/dnsmasq/src
 
-LOCAL_CFLAGS := -O2 -g -W -Wall -D__ANDROID__ -DNO_IPV6 -DNO_TFTP -DNO_SCRIPT -D_BSD_SOURCE
+LOCAL_CFLAGS := -O2 -g -W -Wall -D__ANDROID__ -DNO_IPV6 -DNO_TFTP -DNO_SCRIPT -D_BSD_SOURCE \
+                -Wno-unused-variable -Wno-unused-parameter -Werror
 LOCAL_SYSTEM_SHARED_LIBRARIES := libc
 LOCAL_SHARED_LIBRARIES := libcutils liblog
 

arter97/dnsmasq

src/Android.mk
@@ -10,7 +10,8 @@ LOCAL_MODULE := dnsmasq
 
 LOCAL_C_INCLUDES := external/dnsmasq/src
 
-LOCAL_CFLAGS := -O2 -g -W -Wall -D__ANDROID__ -DIPV6 -DNO_TFTP -DNO_SCRIPT -D_BSD_SOURCE
+LOCAL_CFLAGS := -O2 -g -W -Wall -D__ANDROID__ -DIPV6 -DNO_TFTP -DNO_SCRIPT -D_BSD_SOURCE \
+                -Wno-unused-variable -Wno-unused-parameter -Werror
 LOCAL_SYSTEM_SHARED_LIBRARIES := libc
 LOCAL_SHARED_LIBRARIES := libcutils liblog
 